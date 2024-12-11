ab39d22c8beeaceb986c2db718e3b2a81cbaf4ed
NeilRow56 <neilrowland56@gmail.com>
2024-12-11 17:23:07 +0000

    Create a new task and display on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import NewTodoForm from '@/components/NewTodoForm'
+import TodoList from '@/components/todo-list'
 import React from 'react'
 
 export default function HomePage() {
   return (
     <section className='py-24'>
-      <div className='container max-w-2xl'>
+      <div className='container mx-auto flex w-full max-w-2xl flex-col'>
         <h1 className='font-serif text-3xl font-medium'>
           Keep Track of your tasks
         </h1>
         <NewTodoForm />
+        <TodoList />
       </div>
     </section>
   )
--- src/components/NewTodoForm.tsx
+++ src/components/NewTodoForm.tsx
@@ -1,11 +1,21 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { createTodoAction } from '@/lib/actions'
+import { useActionState } from 'react'
+
 export default function NewTodoForm() {
+  const [state, formAction, insPending] = useActionState(createTodoAction, {
+    success: '',
+    error: undefined
+  })
   return (
-    <form action=''>
+    <form action={formAction}>
+      <div className='text-red-500'>{state.error}</div>
       <div className='mt-12 flex gap-3'>
         <Input type='text' name='title' placeholder='Enter a task' />
-        <Button type='submit'>Add Todo</Button>
+        <Button type='submit'>{insPending ? 'Adding...' : 'Add Task'}</Button>
       </div>
     </form>
   )
