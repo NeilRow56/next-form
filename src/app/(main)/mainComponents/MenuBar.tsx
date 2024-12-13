9fe697c4259f1a6853e981f2b7164ef6862a48b5
NeilRow56 <neilrowland56@gmail.com>
2024-12-13 05:26:40 +0000

    Completed Menu Bar

--- src/app/(main)/mainComponents/MenuBar.tsx
+++ src/app/(main)/mainComponents/MenuBar.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 
-import { Bookmark, Home } from 'lucide-react'
+import { Bell, Bookmark, Home, Mail } from 'lucide-react'
 import Link from 'next/link'
 import { auth } from '@clerk/nextjs/server'
 
@@ -26,8 +26,28 @@ export default async function MenuBar({ className }: MenuBarProps) {
           <span className='hidden lg:inline'>Home</span>
         </Link>
       </Button>
-      <Button>Notifications</Button>
-      <Button>Messages</Button>
+      <Button
+        asChild
+        variant='ghost'
+        className='item-center flex justify-start gap-3'
+        title='notifications'
+      >
+        <Link href='/notifications'>
+          <Bell />
+          <span className='hidden lg:inline'>Notifications</span>
+        </Link>
+      </Button>
+      <Button
+        asChild
+        variant='ghost'
+        className='item-center flex justify-start gap-3'
+        title='messages'
+      >
+        <Link href='/messages'>
+          <Mail />
+          <span className='hidden lg:inline'>Messages</span>
+        </Link>
+      </Button>
 
       <Button
         variant='ghost'
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,15 +2,8 @@
 
 export default function Home() {
   return (
-    <main className='container mx-auto flex min-w-0 gap-5'>
-      <div className='w-full min-w-0 space-y-5'>
-        {/* <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn> */}
-      </div>
+    <main className='flex w-full min-w-0 gap-5'>
+      <div className='w-full min-w-0 space-y-5'>Home</div>
     </main>
   )
 }
